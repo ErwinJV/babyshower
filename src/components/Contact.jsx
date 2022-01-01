b50895f634559c97ce11fe015a6e6e19331c2934
Erwin Jimenez <erwinjv98@gmail.com>
2022-01-01 18:05:29 -0300

    Tooltip incorporado al Boton Whatsapp

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,12 +6,26 @@ import WsLink from "./WsLink";
 const Contact = () => {
   return (
     <div className=" w-100 contact d-flex  align-items-center justify-content-around mt-3 ">
-      <a href="https://wa.link/5w54vp" target={"_blank"} className="d-flex align-self-center text-decoration-none justify-content-center text-secondary">
-        <span className="align-self-center me-3 fs-5 d-none d-md-block">Si tienes alguna inquietud, te puedes comunicar con Mami o Papi por este medio.</span>
-        <WsLink  />
+      <a
+        href="https://wa.link/5w54vp"
+        target={"_blank"}
+        className="d-flex align-self-center text-decoration-none justify-content-center text-secondary position-relative"
+      >
+        <span className="align-self-center me-3 fs-5 d-none d-md-block">
+          Si tienes alguna inquietud, te puedes comunicar con Mami o Papi por
+          este medio.
+        </span>
+        <WsLink />
+        <span className="tooltip-box d-md-none">
+          Si tienes alguna inquietud, te puedes comunicar con Mami o Papi por
+          este medio.
+        </span>
       </a>
 
-      <FontAwesomeIcon icon={faWhatsapp} className="d-none d-md-block icon-ws" />
+      <FontAwesomeIcon
+        icon={faWhatsapp}
+        className="d-none d-md-block icon-ws"
+      />
     </div>
   );
 };
