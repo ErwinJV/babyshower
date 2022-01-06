d6ac3e83b51824afe6b7651f81d88149cb218a51
Erwin Jimenez <erwinjv98@gmail.com>
2022-01-06 14:46:52 -0300

    Paginacion de regalos añadida+

--- src/containers/GIftList.jsx
+++ src/containers/GIftList.jsx
@@ -11,7 +11,7 @@ const GIftList = () => {
   const [gifts, setGifts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [giftsPerPage, setPostPerPage] = useState(10);
+  const [giftsPerPage, setPostPerPage] = useState(4);
   console.log(giftsPerPage);
   useEffect(() => {
     const fetchGifts = async () => {
