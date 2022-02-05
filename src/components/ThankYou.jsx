8a2315771c306b3adec6a18a8588ed90a429251d
Erwin Jimenez <erwinjv98@gmail.com>
2022-02-05 17:24:50 -0400

    Todo listo+++

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -10,7 +10,7 @@ const ThankYou = () => {
   const [minutes, setMinutes] = useState();
   const [days, setDays] = useState();
 
-  const [deadline, setDeadline] = useState("Feb 05 2022 16:00:00 UTC-5");
+  const [deadline, setDeadline] = useState("Feb 05 2022 16:30:00 UTC-5");
 
   const remainTime = (deadline) => {
     let time = new Date();
@@ -20,7 +20,7 @@ const ThankYou = () => {
     let now = new Date(),
       remainTime = (new Date(deadline) - now + 1000) / 1000,
       remainSeconds = ("0" + Math.floor(remainTime % 60)).slice(-2),
-      remainMinutes = ("0" + Math.floor((remainTime / 60) % 60)).slice(-2),
+      remainMinutes = ("0" + Math.floor((remainTime / 60) % 60)).slice(-3),
       remainHours = ("0" + Math.floor((remainTime / 3600) % 24)).slice(-2),
       remainDays = ("0" + Math.floor(remainTime / (3600 * 24))).slice(-2);
 
@@ -48,13 +48,14 @@ const ThankYou = () => {
     const welcomeText = document.getElementById("welcome-text");
     const countdown = document.getElementById("countdown");
     let now = new Date();
-    console.log(hours);
+    console.log(minutes);
 
     if (now.getFullYear() === 2022) {
       if (
         now.getDate() === 5 &&
         now.getMonth() === 1 &&
         hours <= 0 &&
+        minutes <= 0 &&
         now.getHours() < 23
       ) {
         welcomeTitle.innerHTML =
